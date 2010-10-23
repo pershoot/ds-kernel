2694e6ed1516e2cd7350bebf734f7db077864555
Prashant Somashekar <prashsomash@gmail.com>
2010-10-23 09:05:01 -0400

    update with latest cpufreq_ondemand/interactive

--- cpufreq_interactive.c
+++ cpufreq_interactive.c
@@ -52,7 +52,21 @@ static cpumask_t work_cpumask;
  * The minimum ammount of time to spend at a frequency before we can ramp down,
  * default is 50ms.
  */
+
+#if defined (CONFIG_ARCH_MSM7X00A)
+
 #define DEFAULT_MIN_SAMPLE_TIME 35000;
+
+#elif defined (CONFIG_ARCH_MSM_SCORPION)
+
+#define DEFAULT_MIN_SAMPLE_TIME 45000;
+
+#else
+
+#define DEFAULT_MIN_SAMPLE_TIME 50000;
+
+#endif
+
 static unsigned long min_sample_time;
 
 static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
@@ -64,7 +78,21 @@ static
 struct cpufreq_governor cpufreq_gov_interactive = {
 	.name = "interactive",
 	.governor = cpufreq_governor_interactive,
-	.max_transition_latency = 70000000,
+
+#if defined (CONFIG_ARCH_MSM7X00A)
+
+        .max_transition_latency = 7000000,
+
+#elif defined (CONFIG_ARCH_MSM_SCORPION)
+
+        .max_transition_latency = 8000000,
+
+#else
+
+        .max_transition_latency = 10000000,
+
+#endif
+
 	.owner = THIS_MODULE,
 };
 
